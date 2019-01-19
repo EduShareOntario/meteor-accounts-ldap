9c2d96b0c457cc3c86cdf3c11cb24c4ece511145
Todd Hiles <2toddhiles@gmail.com>
2019-01-18 19:48:47 -0500

    Bumped package version and accounts base dependency.

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
 Package.describe({
-  'summary': 'Meteor account login via LDAP using activedirectory.js',
-  'version': '0.1.4',
-  'git' : 'https://github.com/tdamsma/meteor-accounts-ldap',
-  'name' : 'tdamsma:meteor-accounts-ldap'
+  'summary': 'Meteor account login via LDAP using activedirectory.js (coffeescript2)',
+  'version': '0.2.0',
+  'git' : 'https://github.com/EduShareOntario/meteor-accounts-ldap.git',
+  'name' : 'edushareontario:meteor-accounts-ldap'
 });
 
 Npm.depends({'activedirectory' : '0.7.0'});
 
 Package.on_use(function (api) {
+  api.versionsFrom("meteor@1.4.4.5")
   api.use(['coffeescript@2.2.1_1'], 'server');
-  api.use(['accounts-base@1.2.1'], ['client', 'server']);
-  api.imply('accounts-base@1.2.1', ['client', 'server']);
-  api.use(['templating@1.1.4','jquery@1.11.4'], 'client');
+  api.use(['accounts-base@1.4.2'], ['client', 'server']);
+  api.imply('accounts-base@1.4.2', ['client', 'server']);
+  api.use(['templating@1.2.15','jquery@1.11.11'], 'client');
   api.add_files([
     'ldap_client.html',
     'ldap_client.js'], 'client');
